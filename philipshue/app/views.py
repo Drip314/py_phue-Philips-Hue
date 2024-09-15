6fb9a3b463a266ca5ff8571c165d160c67b3040b
phbruemmer <phbruemmer@gmail.com>
2024-09-15 21:23:14 +0200

    - Added input slider for hue value and bri value. - Next up:     - Working on state display for the lights     -> rewrite html structure

--- philipshue/app/views.py
+++ philipshue/app/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 
 
 def myview(request):
+    lights = request.POST.getlist('lamp')
+
     if request.POST.get('main') == "toggle":
-        philip.on_off(request.POST.getlist('lamp'))
+        philip.on_off(lights)
+
+    if request.POST.get('main') == "apply":
+        hue = request.POST.get('hue')
+        bri = request.POST.get('bri')
+        print(hue)
+        print(bri)
+        if hue:
+            philip.change_color(lights, int(hue))
+        if bri:
+            philip.change_brightness(lights, int(bri))
 
     context = {
         'lights': philip.get_lights()
--- philipshue/scripts/philip.py
+++ philipshue/scripts/philip.py
@@ -15,3 +15,20 @@ def on_off(lights):
     if len(lights) == 0:
         return
     b.set_light(lights, 'on', not b.get_light(lights[random.randint(0, len(lights) - 1)], 'on'))
+
+
+def change_brightness(lights, bri):
+    if len(lights) == 0:
+        return
+    b.set_light(lights, 'bri', bri)
+
+
+def change_color(lights, hue):
+    if len(lights) == 0:
+        return
+    b.set_light(lights, 'hue', hue)
+
+
+if __name__ == '__main__':
+    change_brightness([1, 2], 200)
+    change_color([1, 2], 30000)
