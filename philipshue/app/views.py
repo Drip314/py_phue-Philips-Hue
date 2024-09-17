1de2b18cbe80c7a2a0ec126bb8ab75745439b0d1
phbruemmer <phbruemmer@gmail.com>
2024-09-17 22:07:28 +0200

    - rewrote view function for the HueBridge -Class - Added hue, bri and sat as inputs for the bridge methods.

--- philipshue/app/views.py
+++ philipshue/app/views.py
@@ -3,23 +3,23 @@ from django.shortcuts import render
 
 
 def myview(request):
+    bridge = philip.HueBridge("192.168.115.110")
     lights = request.POST.getlist('lamp')
 
     if request.POST.get('main') == "toggle":
-        philip.on_off(lights)
+        bridge.on_off(lights)
 
     if request.POST.get('main') == "apply":
-        color = HexToHue.Color(request.POST.get('rgb'))
-        bri = request.POST.get('bri')
-        print(color)
-        print(bri)
-        if color:
-            philip.change_color(lights, int(color.to_hue()))
-        if bri:
-            philip.change_brightness(lights, int(bri))
+        hue = int(request.POST.get('hue')) # translation not complete
+        bri = int(request.POST.get('bri'))
+        sat = int(request.POST.get('sat'))
+        bridge.change_hue(lights, hue)
+        bridge.change_bri(lights, bri)
+        bridge.change_sat(lights, sat)
 
     context = {
-        'lights': philip.get_lights()
+        'lights': bridge.get_lights(),
+        'light_state': bridge.get_light_state()
     }
 
     return render(request, 'index.html', context=context)
