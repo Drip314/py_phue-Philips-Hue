1bdb1e8c8db84f20180de08c019634991a8d6bf8
phbruemmer <phbruemmer@gmail.com>
2024-09-17 22:05:22 +0200

    - Simplified Code by concluding multiple methods into one. - Added get -methods to get the current state of specific parameters - Added change -methods to change specific parameters.

--- philipshue/scripts/philip.py
+++ philipshue/scripts/philip.py
@@ -15,29 +15,41 @@ class HueBridge:
     def get_lights(self):
         return self.bridge.lights
 
-    def get_light_state(self):
+    def get_parameter_state(self, param):
         lights = self.get_lights()
         light_states = []
         for i in range(0, len(lights)):
-            light_states.append(self.bridge.get_light(lights[i].name, 'on'))
-        print(light_states)
+            light_states.append(self.bridge.get_light(lights[i].name, parameter=param))
+        return light_states
+
+    def change_state(self, lights, param, value):
+        if len(lights) == 0:
+            return
+        return self.bridge.set_light(lights, param, value)
+
+    def get_light_state(self):
+        return self.get_parameter_state('on')
+
+    def get_hue(self):
+        return self.get_parameter_state('hue')
+
+    def get_bri(self):
+        return self.get_parameter_state('bri')
 
     def on_off(self, lights):
-        if len(lights) == 0:
-            return
-        self.bridge.set_light(lights, 'on', not self.bridge.get_light(lights[random.randint(0, len(lights) - 1)], 'on'))
+        self.change_state(lights, 'on', not self.bridge.get_light(lights[random.randint(0, len(lights) - 1)], 'on'))
 
-    def change_brightness(self, lights, bri):
-        if len(lights) == 0:
-            return
-        self.bridge.set_light(lights, 'bri', bri)
+    def change_hue(self, lights, hue):
+        self.change_state(lights, 'hue', hue)
 
-    def change_color(self, lights, hue):
-        if len(lights) == 0:
-            return
-        self.bridge.set_light(lights, 'hue', hue)
+    def change_bri(self, lights, bri):
+        self.change_state(lights, 'bri', bri)
+
+    def change_sat(self, lights, lig):
+        self.change_state(lights, 'sat', lig)
 
 
 if __name__ == '__main__':
     b = HueBridge("192.168.115.110")
     b.get_light_state()
+    b.on_off([1])
