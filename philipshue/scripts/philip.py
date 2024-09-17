80431a09967d3a7deaa997504e31d5dee46593cf
phbruemmer <phbruemmer@gmail.com>
2024-09-17 17:43:11 +0200

    - Modified philip.py with a class and more methods to either control the lights or get the state of the lights.

--- philipshue/scripts/philip.py
+++ philipshue/scripts/philip.py
@@ -2,33 +2,42 @@ from phue import Bridge
 import random
 
 
-b = Bridge('192.168.115.110')
-b.connect()
-b.get_api()
+class HueBridge:
+    def __init__(self, ip):
+        self.ip = str(ip)
+        if not len(self.ip.split('.')) == 4:
+            print("[ERROR] invalid IP.")
+            raise ValueError(f"Invalid IP received.\n-> {self.ip} is not a valid IP!")
+        self.bridge = Bridge(self.ip)
+        self.bridge.connect()
+        self.bridge.get_api()
 
+    def get_lights(self):
+        return self.bridge.lights
 
-def get_lights():
-    return b.lights
+    def get_light_state(self):
+        lights = self.get_lights()
+        light_states = []
+        for i in range(0, len(lights)):
+            light_states.append(self.bridge.get_light(lights[i].name, 'on'))
+        print(light_states)
 
+    def on_off(self, lights):
+        if len(lights) == 0:
+            return
+        self.bridge.set_light(lights, 'on', not self.bridge.get_light(lights[random.randint(0, len(lights) - 1)], 'on'))
 
-def on_off(lights):
-    if len(lights) == 0:
-        return
-    b.set_light(lights, 'on', not b.get_light(lights[random.randint(0, len(lights) - 1)], 'on'))
+    def change_brightness(self, lights, bri):
+        if len(lights) == 0:
+            return
+        self.bridge.set_light(lights, 'bri', bri)
 
-
-def change_brightness(lights, bri):
-    if len(lights) == 0:
-        return
-    b.set_light(lights, 'bri', bri)
-
-
-def change_color(lights, hue):
-    if len(lights) == 0:
-        return
-    b.set_light(lights, 'hue', hue)
+    def change_color(self, lights, hue):
+        if len(lights) == 0:
+            return
+        self.bridge.set_light(lights, 'hue', hue)
 
 
 if __name__ == '__main__':
-    change_brightness([1, 2], 200)
-    change_color([1, 2], 30000)
+    b = HueBridge("192.168.115.110")
+    b.get_light_state()
