141b9d4a4a598a98af51554f8dee5c7ae9a7e597
phbruemmer <phbruemmer@gmail.com>
2024-09-19 22:22:53 +0200

    - Added Light_ids, changed hue, bri and sat calculation and fixed problems when applying the changes

--- philipshue/app/views.py
+++ philipshue/app/views.py
@@ -5,17 +5,25 @@ from django.shortcuts import render
 def myview(request):
     bridge = philip.HueBridge("192.168.115.110")
     lights = request.POST.getlist('lamp')
+    light_ids = []
+    for light in lights:
+        light_ids.append(int(bridge.get_id_by_name(light)))
 
     if request.POST.get('main') == "toggle":
         bridge.on_off(lights)
 
     if request.POST.get('main') == "apply":
-        hue = int(request.POST.get('hue')) # translation not complete
-        bri = int(request.POST.get('bri'))
-        sat = int(request.POST.get('sat'))
-        bridge.change_hue(lights, hue)
-        bridge.change_bri(lights, bri)
-        bridge.change_sat(lights, sat)
+        hue = round((int(request.POST.get('hue')) / 360) * 65535)
+        bri = round((int(request.POST.get('bri')) / 100) * 254)
+        sat = round((int(request.POST.get('sat')) / 100) * 254)
+
+        print(hue)
+        print(bri)
+        print(sat)
+
+        bridge.change_hue(light_ids, hue)
+        bridge.change_bri(light_ids, bri)
+        bridge.change_sat(light_ids, sat)
 
     context = {
         'lights': bridge.get_lights(),
